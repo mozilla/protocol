84d0b4bd2c6088b5752642439f1e5966913efbf1
Michael McCombie <michael@michaelm.cc>
2018-04-23 21:19:34 -0700

    Remove moduleImporter

--- gulp/tasks/sass-compile.js
+++ gulp/tasks/sass-compile.js
@@ -6,7 +6,6 @@ const handleErrors = require('../utils/handleErrors');
 const autoprefixer = require('gulp-autoprefixer');
 const plumber = require('gulp-plumber');
 const merge = require('merge-stream');
-const moduleImporter = require('sass-module-importer');
 
 // Compile the main Protocol Sass file(s) to CSS.
 gulp.task('sass:compile', () => {
@@ -15,7 +14,6 @@ gulp.task('sass:compile', () => {
     Object.keys(config).forEach((key) => {
         let val = config[key];
         tasks.push(gulp.src(val.src)
-            .pipe(sass({ importer: moduleImporter() }))
             .pipe(plumber({ errorHandler: handleErrors }))
             .pipe(sourcemaps.init())
             .pipe(sass(config.settings))
