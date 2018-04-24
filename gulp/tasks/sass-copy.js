d513a942a053e35d8123a4e8cd5157e9429f78e7
Michael McCombie <michael@michaelm.cc>
2018-04-24 14:53:59 -0700

    Replace path reference

--- gulp/tasks/sass-copy.js
+++ gulp/tasks/sass-copy.js
@@ -1,13 +1,17 @@
 const gulp = require('gulp');
 const config = require('../config').sassCopy;
 const merge = require('merge-stream');
+const replace = require('gulp-replace');
 
 // Copy across all original Sass source files for distribution.
 gulp.task('sass:copy', () => {
     let tasks = [];
     Object.keys(config).forEach((key) => {
         let val = config[key];
-        tasks.push(gulp.src(val.src).pipe(gulp.dest(val.dest)));
+        tasks.push(gulp.src(val.src)
+            .pipe(replace('./node_modules/@mozilla-protocol/', ''))
+            .pipe(gulp.dest(val.dest))  
+        );
     });
 
     return merge(tasks);
