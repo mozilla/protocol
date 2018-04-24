9b68de2e8b6c67f213a3785e88fb9dd24faafb60
Michael McCombie <michael@michaelm.cc>
2018-04-24 15:17:13 -0700

    Remove npm package reference from protocol.scss

--- gulp/config.js
+++ gulp/config.js
@@ -84,10 +84,7 @@ module.exports = {
     },
     sassCopy: {
         protocol: {
-            src: [
-                `${src}/sass/protocol/**/*.scss`,
-                `./node_modules/@mozilla-protocol/*/*.scss`
-            ],
+            src: `${src}/sass/protocol/**/*.scss`,
             dest: `${dest}/protocol/protocol/css/`
         },
         docs: {
@@ -96,6 +93,10 @@ module.exports = {
                 `${src}/sass/demos/**/*.scss`
             ],
             dest: `${dest}/docs/css/`
+        },
+        includes: {
+            src: `./node_modules/@mozilla-protocol/*/*.scss`,
+            dest: `${dest}/protocol/protocol/css/includes`
         }
     },
     jsCopy: {
