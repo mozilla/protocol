04b3a6b11c42974e7aacf5b40693bb9c3b9a6c13
Josh Mize <jgmize@gmail.com>
2019-12-11 14:43:22 -0600

    Add .gitlab-ci.yml with build-stage & sync-s3-stage
    For mozmeao/infra#985
    
    Combine build & sync jobs
    
    Add build-sync-prod
    
    Run build container with current user id
    
    Remove Jenkinsfile

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd "$(dirname ${BASH_SOURCE[0]})"/..
+mkdir -p .npm .config
 
 docker run --rm --label=node \
+    -u $(id -u) \
+    -v $(pwd)/.config:/.config \
+    -v $(pwd)/.npm:/.npm \
     -v $(pwd):/app -w /app\
     node:10-alpine \
     npm run-script build
