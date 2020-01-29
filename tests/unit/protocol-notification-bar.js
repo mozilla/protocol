d1c1fa6a890def16195aaf8b0e923ef2f0d259e0
Steve Jalim <steve@somefantastic.co.uk>
2020-01-29 22:36:05 +0000

    Support an optional CTA link in a JS-created NotificationBar
    This changeset adds support for an optional hyperlink added after the main text in a JS-triggered NotificationBar component.
    
    An optional options object lets the developer specify the text for inside the link, the URL for its href and, optionally, another object of additional attributes which are all then applied to the link.
    
    Includes tests.
    
    Resolves https://github.com/mozilla/protocol/issues/460

--- tests/unit/protocol-notification-bar.js
+++ tests/unit/protocol-notification-bar.js
@@ -68,4 +68,126 @@ describe('protocol-notification.js', function() {
         });
 
     });
+
+    describe('interactions-with-cta-no-attrs', function () {
+
+        var options = {
+            open: function () {}, // eslint-disable-line no-empty-function
+            close: function () {} // eslint-disable-line no-empty-function
+        };
+
+        beforeEach(function () {
+            spyOn(options, 'open');
+            spyOn(options, 'close');
+
+            var button = document.querySelector('.mzp-c-button');
+
+            button.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                Mzp.Notification.init(button, {
+                    cta: {
+                        text: 'This is a call to action',
+                        url: 'https://example.com/test/url',
+                    },
+                    hasDismiss: true,
+                    onNotificationOpen: options.open,
+                    onNotificationClose: options.close
+                });
+            });
+
+            button.click();
+        });
+
+        afterEach(function(){
+            var node = document.querySelector('.mzp-c-notification-bar');
+            if (node) {
+                node.parentNode.removeChild(node);
+            }
+        });
+
+        it('opens as expected, with a CTA', function() {
+            expect(options.open).toHaveBeenCalled();
+
+            var notification = document.querySelector('.mzp-c-notification-bar');
+            expect(notification).toBeTruthy();
+
+            var cta = document.querySelector('.mzp-c-notification-bar-cta');
+            expect(cta).toBeTruthy();
+        });
+
+        it('closes as expected', function() {
+            var dismissButton = document.querySelector('.mzp-c-notification-bar-button');
+            dismissButton.click();
+            expect(options.close).toHaveBeenCalled();
+
+            var notification = document.querySelector('.mzp-c-notification-bar');
+            expect(notification).toEqual(null);
+        });
+    });
+
+    describe('interactions-with-cta-extra-attrs', function () {
+
+        var options = {
+            open: function () {}, // eslint-disable-line no-empty-function
+            close: function () {} // eslint-disable-line no-empty-function
+        };
+
+        beforeEach(function () {
+            spyOn(options, 'open');
+            spyOn(options, 'close');
+
+            var button = document.querySelector('.mzp-c-button');
+
+            button.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                Mzp.Notification.init(button, {
+                    cta: {
+                        text: 'This is a call to action',
+                        url: 'https://example.com/test/url',
+                        attrs: {
+                            'target': '_blank',
+                            'rel': 'nofollow',
+                            'hreflang': 'en-US',
+                        }
+                    },
+                    hasDismiss: true,
+                    onNotificationOpen: options.open,
+                    onNotificationClose: options.close
+                });
+            });
+
+            button.click();
+        });
+
+        afterEach(function(){
+            var node = document.querySelector('.mzp-c-notification-bar');
+            if (node) {
+                node.parentNode.removeChild(node);
+            }
+        });
+
+        it('opens as expected, with a CTA featuring the extra attributes', function() {
+            expect(options.open).toHaveBeenCalled();
+
+            var notification = document.querySelector('.mzp-c-notification-bar');
+            expect(notification).toBeTruthy();
+
+            var cta = document.querySelector('.mzp-c-notification-bar-cta');
+            expect(cta).toBeTruthy();
+            expect(cta.getAttribute('target')).toBe('_blank');
+            expect(cta.getAttribute('rel')).toBe('nofollow');
+            expect(cta.getAttribute('hreflang')).toBe('en-US');
+        });
+
+        it('closes as expected', function() {
+            var dismissButton = document.querySelector('.mzp-c-notification-bar-button');
+            dismissButton.click();
+            expect(options.close).toHaveBeenCalled();
+
+            var notification = document.querySelector('.mzp-c-notification-bar');
+            expect(notification).toEqual(null);
+        });
+    });
 });
