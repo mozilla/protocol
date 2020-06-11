adcfbdb861474011e21fcedf7d25cd0c35b05c76
Errolyn <Errolyn@mozilla.com>
2020-06-11 15:32:10 -0700

    feedback changes

--- gulpfile.js/tasks/drizzle.js
+++ gulpfile.js/tasks/drizzle.js
@@ -12,7 +12,7 @@ function renderIcons(iconData, depth=2){
         .forEach( (key) => {
             output += `<h${depth} class="icon-title">${key}</h${depth}>`;
             iconData[key].files.forEach( (file) => {
-                output += `<div class="img-group" data-clipboard-text="img/icons/${file.src}"><img src="/assets/protocol/protocol/img/icons/${file.src}" class="icons"/>`;
+                output += `<div class="img-group" title="Click to copy file path" data-clipboard-text="img/icons/${file.src}"><img src="/assets/protocol/protocol/img/icons/${file.src}" class="icons"/>`;
                 output += `<p class="icon-name">${file.name}</p><p class="copied">Copied!</p></div>`;
             });
             iconData[key].subsets.forEach((subset) => {
