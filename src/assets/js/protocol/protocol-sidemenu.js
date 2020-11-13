6d723619c53bd78d90b6589025fe9320c96665c0
Stephanie Hobson <sjhobson@gmail.com>
2020-11-12 16:46:16 -0800

    Check for sidemenu before initializing (#643)

--- src/assets/js/protocol/protocol-sidemenu.js
+++ src/assets/js/protocol/protocol-sidemenu.js
@@ -9,18 +9,21 @@
     var menuMain = document.querySelector('.mzp-c-sidemenu-main');
     var menuToggle = document.querySelector('.mzp-js-toggle');
 
-    // Make the menu more accessible
-    menuToggle.setAttribute('tabindex', '0');
-    menuToggle.setAttribute('role', 'button');
-    menuMain.setAttribute('aria-expanded', 'false');
+    if(menu && menuMain && menuToggle) {
 
-    // Toggle the sidebar menu
-    menuToggle.addEventListener('click', function(e) {
-        e.preventDefault();
-        menu.classList.toggle('mzp-is-active');
+        // Make the menu more accessible
+        menuToggle.setAttribute('tabindex', '0');
+        menuToggle.setAttribute('role', 'button');
+        menuMain.setAttribute('aria-expanded', 'false');
 
-        var menuExpanded = menuMain.getAttribute('aria-expanded') === 'true' ? 'false' : 'true';
-        menuMain.setAttribute('aria-expanded', menuExpanded);
-    }, false);
+        // Toggle the sidebar menu
+        menuToggle.addEventListener('click', function(e) {
+            e.preventDefault();
+            menu.classList.toggle('mzp-is-active');
+
+            var menuExpanded = menuMain.getAttribute('aria-expanded') === 'true' ? 'false' : 'true';
+            menuMain.setAttribute('aria-expanded', menuExpanded);
+        }, false);
+    }
 
 })();
